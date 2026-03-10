4c130b06e8af9ce977aa1f5633bedf174d8e788f
Júlia Galvão <juliagalvaoalmeidasantos@gmail.com>
2026-03-10 20:06:28 -0300

    Atualização do Menu, com add de dados pessoais

--- Menu.ts
+++ Menu.ts
@@ -1,73 +1,114 @@
-import readline = require("readline-sync");
+import readlinesync = require("readline-sync");
 import { Colors } from "./src/util/Colors";
 
-console.log(Colors.fgGreen + "BANCO DO BRAZIL COM Z" + Colors.reset);
-let opcao: number = 0;
+export function main() {
+  let opcao: number;
 
-while (opcao != 9) {
-    console.log(
-Colors.fgYellow + "\n************************************************************" + Colors.reset +
-"\n" + Colors.fgGreen + "                 BANCO DO BRAZIL COM Z" + Colors.reset +
-"\n" + Colors.fgYellow + "************************************************************" + Colors.reset +
-"\n\n" + Colors.fgCyan +
-"1 - Criar Conta\n" +
-"2 - Listar todas as Contas\n" +
-"3 - Buscar Conta por Numero\n" +
-"4 - Atualizar Dados da Conta\n" +
-"5 - Apagar Conta\n" +
-"6 - Sacar\n" +
-"7 - Depositar\n" +
-"8 - Transferir valores entre Contas\n" +
-"9 - Sair\n" +
-Colors.reset +
-"\n" + Colors.fgYellow + "************************************************************" + Colors.reset +
-"\n" + Colors.fgWhite + "Entre com a opção desejada:\n" + Colors.reset
-);;
+  while (true) {
+    Colors.fgYellow +
+      console.log("*****************************************************");
+    +Colors.reset;
+    console.log("                                                     ");
+    Colors.fgGreen +
+      console.log("                BANCO DO BRAZIL COM Z                ");
+    +Colors.reset;
+    console.log("                                                     ");
+    Colors.fgYellow +
+      console.log("*****************************************************");
+    +Colors.reset;
+    console.log("                                                     ");
+    Colors.fgCyan +
+      console.log("            1 - Criar Conta                          ");
+    +Colors.reset;
+    Colors.fgCyan +
+      console.log("            2 - Listar todas as Contas               ");
+    +Colors.reset;
+    Colors.fgCyan +
+      console.log("            3 - Buscar Conta por Numero              ");
+    +Colors.reset;
+    Colors.fgCyan +
+      console.log("            4 - Atualizar Dados da Conta             ");
+    +Colors.reset;
+    Colors.fgCyan +
+      console.log("            5 - Apagar Conta                         ");
+    +Colors.reset;
+    Colors.fgCyan +
+      console.log("            6 - Sacar                                ");
+    +Colors.reset;
+    Colors.fgCyan +
+      console.log("            7 - Depositar                            ");
+    +Colors.reset;
+    Colors.fgCyan +
+      console.log("            8 - Transferir valores entre Contas      ");
+    +Colors.reset;
+    Colors.fgCyan +
+      console.log("            9 - Sair                                 ");
+    +Colors.reset;
+    Colors.fgCyan +
+      console.log("                                                     ");
+    Colors.fgYellow +
+      console.log("*****************************************************");
+    +Colors.reset;
+    console.log("                                                     ");
 
-    opcao = readline.questionInt("Digite um número: ")
+    console.log("Entre com a opção desejada: ");
+    opcao = readlinesync.questionInt("");
 
-    switch (opcao) {
-
-        case 1:
-            console.log("\nOpção selecionada: CRIAR CONTA")
-            break;
-
-        case 2:
-            console.log("\nOpção selecionada: LISTAR TODAS AS CONTAS")
-            break;
-
-        case 3:
-            console.log("\nOpção selecionada: BUSCAR CONTA POR NÚMERO")
-            break;
-
-        case 4:
-            console.log("\nOpção selecionada: ATUALIZAR DADOS DA CONTA")
-            break;
-
-
-        case 5:
-            console.log("\nOpção selecionada: APAGAR CONTA")
-            break;
-
-        case 6:
-            console.log("\nOpção selecionada: SACAR")
-            break;
-
-        case 7:
-            console.log("\nOpção selecionada: DEPOSITAR")
-            break;
-
-        case 8:
-            console.log("\nOpção selecionada: TRANSFERIR VALORES ENTRE CONTAS")
-            break;
-
-        case 9:
-            console.log("\nOpção selecionada: SAIR")
-            break;
-        default:
-            console.log("\nOpção inválida, tente novamente!")
-            break;
-
+    if (opcao == 9) {
+      console.log("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+      sobre();
+      process.exit(0);
     }
 
+    switch (opcao) {
+      case 1:
+        console.log("\n\nCriar Conta\n\n");
+
+        break;
+      case 2:
+        console.log("\n\nListar todas as Contas\n\n");
+
+        break;
+      case 3:
+        console.log("\n\nConsultar dados da Conta - por número\n\n");
+
+        break;
+      case 4:
+        console.log("\n\nAtualizar dados da Conta\n\n");
+
+        break;
+      case 5:
+        console.log("\n\nApagar uma Conta\n\n");
+
+        break;
+      case 6:
+        console.log("\n\nSaque\n\n");
+
+        break;
+      case 7:
+        console.log("\n\nDepósito\n\n");
+
+        break;
+      case 8:
+        console.log("\n\nTransferência entre Contas\n\n");
+
+        break;
+      default:
+        console.log("\nOpção Inválida!\n");
+
+        break;
+    }
+  }
 }
+
+/* Função com os dados da pessoa desenvolvedora */
+
+export function sobre(): void {
+  console.log("\n*****************************************************");
+  console.log("Projeto Desenvolvido por: Júlia Santos");
+  console.log("Generation Brasil - generation@generation.org");
+  console.log("github.com/juliagasantos");
+  console.log("*****************************************************");
+}
+
+main();
