d225cf54fcf41a1a09b8e00e1477610550f20c01
Júlia Santos <juliagalvaoalmeidasantos@gmail.com>
2026-03-15 22:41:34 -0300

    feat: implementa métodos buscarNoArray, atualizar e deletar no ContaController

--- Menu.ts
+++ Menu.ts
@@ -6,33 +6,66 @@ import { ContaPoupanca } from "./src/model/ContaPoupanca";
 import { ContaController } from "./src/controller/ContaController";
 
 export function main() {
-
   //Instância da Classe ContaController
   let contas: ContaController = new ContaController();
 
   //Variáveis Auxiliares
   let opcao, numero, agencia, tipo, saldo, limite, aniversario: number;
   let titular: string;
-  const tiposContas = ['Conta Corrente', 'Conta poupanca']
+  const tiposContas = ["Conta Corrente", "Conta poupanca"];
   console.log("\nCriar Contas\n");
 
-  let cc1: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000, 100.0);
+  let cc1: ContaCorrente = new ContaCorrente(
+    contas.gerarNumero(),
+    123,
+    1,
+    "João da Silva",
+    1000,
+    100.0,
+  );
   contas.cadastrar(cc1);
 
-  let cc2: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000, 100.0);
+  let cc2: ContaCorrente = new ContaCorrente(
+    contas.gerarNumero(),
+    124,
+    1,
+    "Maria da Silva",
+    2000,
+    100.0,
+  );
   contas.cadastrar(cc2);
 
-  let cp1: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000, 12);
+  let cp1: ContaPoupanca = new ContaPoupanca(
+    contas.gerarNumero(),
+    125,
+    2,
+    "Mariana dos Santos",
+    4000,
+    12,
+  );
   contas.cadastrar(cp1);
 
-  let cp2: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Juliana Ramos", 8000, 15);
+  let cp2: ContaPoupanca = new ContaPoupanca(
+    contas.gerarNumero(),
+    125,
+    2,
+    "Juliana Ramos",
+    8000,
+    15,
+  );
   contas.cadastrar(cp2);
 
   contas.listarTodas();
 
-
   // Objeto da Classe ContaCorrente (Teste)
-  const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
+  const contacorrente: ContaCorrente = new ContaCorrente(
+    2,
+    123,
+    1,
+    "Mariana",
+    15000,
+    1000,
+  );
   contacorrente.visualizar();
   contacorrente.sacar(2000);
   contacorrente.visualizar();
@@ -40,7 +73,14 @@ export function main() {
   contacorrente.visualizar();
 
   // Objeto da Classe ContaPoupanca (teste)
-  const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+  const contapoupanca: ContaPoupanca = new ContaPoupanca(
+    3,
+    123,
+    2,
+    "Victor",
+    1000,
+    10,
+  );
   contapoupanca.visualizar();
   contapoupanca.sacar(200);
   contapoupanca.visualizar();
@@ -48,25 +88,77 @@ export function main() {
   contapoupanca.visualizar();
 
   while (true) {
-    console.log(Colors.fgYellow + "*****************************************************" + Colors.reset);
+    console.log(
+      Colors.fgYellow +
+        "*****************************************************" +
+        Colors.reset,
+    );
     console.log("                                                     ");
-    console.log(Colors.fgGreen + "                BANCO DO BRAZIL COM Z                " + Colors.reset);
+    console.log(
+      Colors.fgGreen +
+        "                BANCO DO BRAZIL COM Z                " +
+        Colors.reset,
+    );
     console.log("                                                     ");
-    console.log(Colors.fgYellow + "*****************************************************" + Colors.reset);
+    console.log(
+      Colors.fgYellow +
+        "*****************************************************" +
+        Colors.reset,
+    );
     console.log("                                                     ");
 
-    console.log(Colors.fgCyan + "            1 - Criar Conta                          " + Colors.reset);
-    console.log(Colors.fgCyan + "            2 - Listar todas as Contas               " + Colors.reset);
-    console.log(Colors.fgCyan + "            3 - Buscar Conta por Numero              " + Colors.reset);
-    console.log(Colors.fgCyan + "            4 - Atualizar Dados da Conta             " + Colors.reset);
-    console.log(Colors.fgCyan + "            5 - Apagar Conta                         " + Colors.reset);
-    console.log(Colors.fgCyan + "            6 - Sacar                                " + Colors.reset);
-    console.log(Colors.fgCyan + "            7 - Depositar                            " + Colors.reset);
-    console.log(Colors.fgCyan + "            8 - Transferir valores entre Contas      " + Colors.reset);
-    console.log(Colors.fgCyan + "            9 - Sair                                 " + Colors.reset);
+    console.log(
+      Colors.fgCyan +
+        "            1 - Criar Conta                          " +
+        Colors.reset,
+    );
+    console.log(
+      Colors.fgCyan +
+        "            2 - Listar todas as Contas               " +
+        Colors.reset,
+    );
+    console.log(
+      Colors.fgCyan +
+        "            3 - Buscar Conta por Numero              " +
+        Colors.reset,
+    );
+    console.log(
+      Colors.fgCyan +
+        "            4 - Atualizar Dados da Conta             " +
+        Colors.reset,
+    );
+    console.log(
+      Colors.fgCyan +
+        "            5 - Apagar Conta                         " +
+        Colors.reset,
+    );
+    console.log(
+      Colors.fgCyan +
+        "            6 - Sacar                                " +
+        Colors.reset,
+    );
+    console.log(
+      Colors.fgCyan +
+        "            7 - Depositar                            " +
+        Colors.reset,
+    );
+    console.log(
+      Colors.fgCyan +
+        "            8 - Transferir valores entre Contas      " +
+        Colors.reset,
+    );
+    console.log(
+      Colors.fgCyan +
+        "            9 - Sair                                 " +
+        Colors.reset,
+    );
 
     console.log("                                                     ");
-    console.log(Colors.fgYellow + "*****************************************************" + Colors.reset);
+    console.log(
+      Colors.fgYellow +
+        "*****************************************************" +
+        Colors.reset,
+    );
     console.log("                                                     ");
 
     console.log("Entre com a opção desejada: ");
@@ -99,7 +191,14 @@ export function main() {
             console.log("Digite o Limite da Conta (R$): ");
             limite = readlinesync.questionFloat("");
             contas.cadastrar(
-              new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite)
+              new ContaCorrente(
+                contas.gerarNumero(),
+                agencia,
+                tipo,
+                titular,
+                saldo,
+                limite,
+              ),
             );
             break;
 
@@ -107,30 +206,113 @@ export function main() {
             console.log("Digite o Dia do aniversário da Conta Poupança: ");
             aniversario = readlinesync.questionInt("");
             contas.cadastrar(
-              new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario)
+              new ContaPoupanca(
+                contas.gerarNumero(),
+                agencia,
+                tipo,
+                titular,
+                saldo,
+                aniversario,
+              ),
             );
             break;
         }
         break;
 
-        keyPress()
+        keyPress();
         break;
       case 2:
         console.log("\n\nListar todas as Contas\n\n");
-        contas.listarTodas()
+        contas.listarTodas();
 
         break;
       case 3:
-        console.log("\n\nConsultar dados da Conta - por número\n\n");
+        console.log(
+          Colors.fgWhite,
+          "\nConsultar dados da Conta - por número\n",
+          Colors.reset,
+        );
+        console.log("Digite o número da conta: ");
+        numero = readlinesync.questionInt("");
+        contas.procurarPorNumero(numero);
+        keyPress();
 
         break;
+
       case 4:
-        console.log("\n\nAtualizar dados da Conta\n\n");
+        console.log(
+          Colors.fgWhite,
+          "\n\nAtualizar dados da Conta\n\n",
+          Colors.reset,
+        );
 
+        console.log("Digite o número da Conta: ");
+        numero = readlinesync.questionInt("");
+
+        let conta = contas.buscarNoArray(numero);
+
+        if (conta != null) {
+          console.log("Digite o Número da agência: ");
+          agencia = readlinesync.questionInt("");
+
+          console.log("Digite o Nome do Titular da conta: ");
+          titular = readlinesync.question("");
+
+          tipo = conta.tipo;
+
+          console.log("\nDigite o Saldo da conta (R$): ");
+          saldo = readlinesync.questionFloat("");
+
+          switch (tipo) {
+            case 1:
+              console.log("Digite o Limite da Conta (R$): ");
+              limite = readlinesync.questionFloat("");
+              contas.atualizar(
+                new ContaCorrente(
+                  numero,
+                  agencia,
+                  tipo,
+                  titular,
+                  saldo,
+                  limite,
+                ),
+              );
+              break;
+
+            case 2:
+              console.log("Digite o dia do aniversário da Conta Poupança: ");
+              aniversario = readlinesync.questionInt("");
+              contas.atualizar(
+                new ContaPoupanca(
+                  numero,
+                  agencia,
+                  tipo,
+                  titular,
+                  saldo,
+                  aniversario,
+                ),
+              );
+              break;
+          }
+        } else {
+          console.log(
+            Colors.fgRed,
+            "\nA Conta numero: " + numero + " não foi encontrada!",
+            Colors.reset,
+          );
+        }
+
+        keyPress();
         break;
-      case 5:
-        console.log("\n\nApagar uma Conta\n\n");
 
+      case 5:
+        console.log(Colors.fgWhite, "\n\nApagar uma Conta\n\n", Colors.reset);
+
+        console.log("Digite o número da Conta: ");
+        numero = readlinesync.questionInt("");
+        contas.deletar(numero);
+
+        keyPress();
         break;
       case 6:
         console.log("\n\nSaque\n\n");
@@ -144,16 +326,13 @@ export function main() {
         console.log("\n\nTransferência entre Contas\n\n");
 
       case 9:
-        console.log("\nOpção selecionada: SAIR")
+        console.log("\nOpção selecionada: SAIR");
         break;
       default:
-        console.log("\nOpção inválida, tente novamente!")
+        console.log("\nOpção inválida, tente novamente!");
         break;
-
     }
-
   }
-
 }
 export function sobre(): void {
   console.log("\n*****************************************************");
--- src/controller/ContaController.ts
+++ src/controller/ContaController.ts
@@ -3,38 +3,87 @@ import { ContaRepository } from "../repository/ContaRepository";
 import { Colors } from "../util/Colors";
 
 export class ContaController implements ContaRepository {
+  private listaContas: Array<Conta> = new Array<Conta>();
+  numero: number = 0;
 
-    private listaContas: Array<Conta> = new Array<Conta>();
-    numero: number = 0;
+  procurarPorNumero(numero: number): void {
+    let buscaConta = this.buscarNoArray(numero);
 
-    procurarPorNumero(numero: number): void {
-        throw new Error("Method not implemented.");
+    if (buscaConta != null) {
+      buscaConta.visualizar();
+    } else
+      console.log(
+        Colors.fgRed,
+        `\nA conta número: ${numero} não foi encontrada!`,
+        Colors.reset,
+      );
+  }
+
+  listarTodas(): void {
+    for (let conta of this.listaContas) {
+      conta.visualizar();
     }
-    listarTodas(): void {
-        for (let conta of this.listaContas) {
-            conta.visualizar();
-        }
+  }
+
+  cadastrar(conta: Conta): void {
+    this.listaContas.push(conta);
+    console.log(
+      Colors.fgGreen,
+      `A Conta número: ${conta.numero} foi criada com sucesso!`,
+      Colors.reset,
+    );
+  }
+
+  atualizar(conta: Conta): void {
+    let buscaConta = this.buscarNoArray(conta.numero);
+
+    if (buscaConta != null) {
+      this.listaContas[this.listaContas.indexOf(buscaConta)] = conta;
+      console.log(
+        Colors.fgGreen,
+        "\nA Conta numero: " + conta.numero + " foi atualizada com sucesso!",
+        Colors.reset,
+      );
+    } else
+      console.log(
+        Colors.fgRed,
+        "\nA Conta numero: " + conta.numero + " não foi encontrada!",
+        Colors.reset,
+      );
+  }
+
+ deletar(numero: number): void {
+  let buscaConta = this.buscarNoArray(numero);
+
+  if (buscaConta != null) {
+    this.listaContas.splice(this.listaContas.indexOf(buscaConta), 1);
+    console.log(Colors.fgGreen,"\nA Conta numero: " + numero +
+      " foi apagada com sucesso!", Colors.reset);
+  } else
+    console.log(Colors.fgRed,"\nA Conta numero: " + numero +
+      " não foi encontrada!", Colors.reset);
+}
+
+  sacar(numero: number, valor: number): void {
+    throw new Error("Method not implemented.");
+  }
+
+  depositar(numero: number, valor: number): void {
+    throw new Error("Method not implemented.");
+  }
+
+  transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
+    throw new Error("Method not implemented.");
+  }
+
+  public gerarNumero(): number {
+    return ++this.numero;
+  }
+
+  public buscarNoArray(numero: number): Conta | null {
+    for (let conta of this.listaContas) {
+      if (conta.numero === numero) return conta;
     }
-    cadastrar(conta: Conta): void {
-        this.listaContas.push(conta);
-        console.log(Colors.fgGreen, `A Conta número: ${conta.numero} foi criada com sucesso!`, Colors.reset)
-    }
-    atualizar(conta: Conta): void {
-        throw new Error("Method not implemented.");
-    }
-    deletar(numero: number): void {
-        throw new Error("Method not implemented.");
-    }
-    sacar(numero: number, valor: number): void {
-        throw new Error("Method not implemented.");
-    }
-    depositar(numero: number, valor: number): void {
-        throw new Error("Method not implemented.");
-    }
-    transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
-        throw new Error("Method not implemented.");
-    }
-    public gerarNumero(): number {
-        return ++this.numero;
-    }
+    return null;
+  }
 }
